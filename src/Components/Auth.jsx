a3df386ba60d3468616eaeaed81af09c905329d0
yash7xm <yash.7xm@gmail.com>
2023-12-08 14:54:25 +0530

    logIn added

--- src/Components/Auth.jsx
+++ src/Components/Auth.jsx
@@ -6,9 +6,71 @@ const Auth = () => {
   const [password, setPassword] = useState("");
   const [regMode, setRegMode] = useState(false);
 
-  const handleRegisterBtn = () => {
-    setRegMode((prevState) => !prevState);
+  const handleRegisterBtn = async () => {
+    try {
+      const checkUser = await fetch("http://localhost:8080/checkUserExist", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username }),
+      });
+      if (checkUser.ok) {
+        document.querySelector(".uname input").value = "";
+        document.querySelector(".pass input").value = "";
+        document.querySelector(".uname input").placeholder =
+          "username already exist";
+        return;
+      }
+    } catch (err) {
+      console.log("error in check user exist");
+    }
+
+    try {
+      const response = await fetch("http://localhost:8080/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, password }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Did not created the user");
+      } else{
+        console.log("created");
+        setRegMode((prevState) => !prevState)
+      } 
+    } catch (err) {
+      console.log(err);
+    }
   };
+
+  const handleLogInBtn = async () => {
+    try {
+      const response = await fetch("http://localhost:8080/signIn", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, password }),
+      });
+
+      if(!response.ok) {
+        document.querySelector(".uname input").value = "";
+        document.querySelector(".pass input").value = "";
+        document.querySelector(".uname input").placeholder =
+          "Invalid username or password";
+        return;
+      }
+      else {
+        setRegMode((prevState) => !prevState)
+      }
+    }
+    catch (err) {
+      console.log('login error');
+    }
+  }
 
   const handleAuthBtn = async () => {
     if (username.length == 0) {
@@ -19,21 +81,12 @@ const Auth = () => {
       document.querySelector(".pass input").placeholder =
         "Enter valid password";
     }
-    if (username.length != 0 && password.length != 0) {
-      try {
-        const response = await fetch("http://localhost:8080/register", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({username, password}),
-        });
 
-        if (!response.ok) {
-          throw new Error("Did not created the user");
-        } else console.log("created");
-      } catch (err) {
-        console.log(err);
+    if (username.length != 0 && password.length != 0) {
+      if (document.querySelector(".auth-btn").textContent === "Register") {
+        handleRegisterBtn();
+      } else {
+        handleLogInBtn();
       }
     }
   };
@@ -69,7 +122,10 @@ const Auth = () => {
         <div className="auth-btn" onClick={handleAuthBtn}>
           {regMode ? "Register" : "LogIn"}
         </div>
-        <div className="footer" onClick={handleRegisterBtn}>
+        <div
+          className="footer"
+          onClick={() => setRegMode((prevState) => !prevState)}
+        >
           {regMode ? "Already a user" : "Create Account"}
         </div>
       </div>
